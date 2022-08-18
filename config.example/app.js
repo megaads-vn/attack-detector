5ae97ceafbfe490a54c622097a9baaa11e46df43
KIMTUNG <nguyenkim08@gmail.com>
2022-08-18 12:01:36 +0700

    fix: comment file config.example

--- config.example/app.js
+++ config.example/app.js
@@ -4,8 +4,8 @@ module.exports = {
         keyword_in_url: "on",
         ip: "on"
     }, 
-    path: "/Users/nguyenkimtung/Downloads/logfile/test/access_log",
-    number_line: 5000,
+    path: "/Users/nguyenkimtung/Downloads/logfile/test/access_log", // đường dẫn tới file access_log
+    number_line: 5000, // số dòng đọc cuối file để parse log
     ip_ignore: [],
     keywords: [
         "page=", 
